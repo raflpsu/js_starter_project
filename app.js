cadbe9d7e4d142a42ce163f63e93da9ffad05683
Rafael Lopes <raflps@icloud.com>
2025-02-03 00:30:10 -0300

    update app.js v 0.2.1

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 alert("Boas vindas ao Jogo do Número Secreto!");
-let secretNumber = Math.floor(Math.random() * 100) + 1;
+let maxNumber = parseInt(prompt("Qual o seu nível de dificuldade? Digite o valor máximo para o número secreto:"));
+let secretNumber = Math.floor(Math.random() * maxNumber) + 1;
 alert(secretNumber);
 let attempts = 0;
 let userNumber = 0;
-let message = "Tente adivinhar o número secreto! \nDica: Ele está entre 1 e 100.";
+let message = `Tente adivinhar o número secreto! \nDica: Ele está entre 1 e ${maxNumber}.`;
 
 while (userNumber !== secretNumber) {
     userNumber = parseInt(prompt(message));
