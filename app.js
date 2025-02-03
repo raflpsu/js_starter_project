3d61c717d9b88aa4d4d68331c8980a7718c33ad5
Rafael Lopes <raflps@icloud.com>
2025-02-02 21:47:04 -0300

    update commit v 0.2

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ alert("Boas vindas ao Jogo do Número Secreto!");
 let secretNumber = Math.floor(Math.random() * 100) + 1;
 alert(secretNumber);
 let attempts = 0;
-let guess = 0;
-let message = "Tente adivinhar o número secreto!";
+let userNumber = 0;
+let message = "Tente adivinhar o número secreto! \nDica: Ele está entre 1 e 100.";
 
-while (guess !== secretNumber) {
-    guess = parseInt(prompt(message));
+while (userNumber !== secretNumber) {
+    userNumber = parseInt(prompt(message));
     attempts++;
-    if (guess > secretNumber) {
+    if (userNumber > secretNumber) {
         message = "O número secreto é menor!";
-    } else if (guess < secretNumber) {
+    } else if (userNumber < secretNumber) {
         message = "O número secreto é maior!";
     }
 }
+
+// operador ternário
+// condição ? valor_se_verdadeiro : valor_se_falso
+
+let changeAttemptsText = attempts === 1 ? "tentativa" : "tentativas";
+alert(message = `Parabéns! Você acertou o número secreto ${secretNumber} em ${attempts} ${changeAttemptsText}.`);
